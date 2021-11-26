0f10e923e566592cc75a58817da65e632add5fb3
Gogee90 <dead69-not@yandex.ru>
2021-11-26 20:48:03 +0500

    It would not create passwords, so i had to add create method to serializer.

--- account/serializers.py
+++ account/serializers.py
@@ -6,3 +6,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data['username'], password=validated_data['password'],
+                                        email=validated_data['email'])
+        user.first_name = validated_data['first_name']
+        user.last_name = validated_data['last_name']
+        user.avatar = validated_data['avatar']
+        user.gender = validated_data['gender']
+        return user
